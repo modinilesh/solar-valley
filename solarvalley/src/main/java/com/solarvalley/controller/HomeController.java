44fbee85a85551a24ad1e7ca270964ddce145ce8
Nilesh Modi <nilesh.modi.nitb@gmail.com>
2024-11-30 16:13:36 +0530

    production changes for logo

--- solarvalley/src/main/java/com/solarvalley/controller/HomeController.java
+++ solarvalley/src/main/java/com/solarvalley/controller/HomeController.java
@@ -6,7 +6,9 @@ import com.solarvalley.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -15,11 +17,15 @@ public class HomeController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 
+    @Value("${logoUrl}")
+    private String logoUrl;
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("logoUrl", logoUrl);
         return "home";
     }
 
